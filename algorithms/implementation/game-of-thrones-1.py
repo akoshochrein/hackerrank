78431a380a0538d328968e8f0904796f58e0cd31
akoshochrein <hoch.akos@gmail.com>
2024-01-24 15:48:31 +0100

    game of thrones 1

---
+++ algorithms/implementation/game-of-thrones-1.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+
+def game_of_thrones_one(word: str) -> str:
+    letter_frequencies = Counter(word)
+
+    odd_letters = 0
+    for letter in letter_frequencies.keys():
+        if letter_frequencies[letter] % 2 != 0:
+            odd_letters += 1
+
+    if odd_letters > 1:
+        return "NO"
+
+    return "YES"
+
+
+print(game_of_thrones_one("cdcdcdcdeeeef"))
+
+
+# anagram: cat -> tac -> atc -> act -> tca -> cta
+# palindrome: tacocat, anna
